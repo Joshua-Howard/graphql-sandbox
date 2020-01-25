fe8d654ef84602a05f359ddb90e80382f60bd0cf
Josh <Joshua-Howard@users.noreply.github.com>
2020-01-25 14:46:03 -0500

    Initial Postgres Model

---
+++ Backend/models/postgresModel.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+
+const PG_URI = 'postgresql://josh@localhost/graphql_sandbox';
+
+// Create a new Postgres connection pool
+const pool = new Pool({
+  connectionString: PG_URI
+});
+
+// // Test Query
+// pool.query('SELECT * FROM test', null, (err, result) => {
+//   console.log(result.rows);
+// });
+
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('Query', text);
+    return pool.query(text, params, callback);
+  }
+};
