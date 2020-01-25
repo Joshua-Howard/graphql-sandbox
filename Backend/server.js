ad6eb79ff4dfaaabb1b36a3c5408add61a79d30e
Josh <Joshua-Howard@users.noreply.github.com>
2020-01-25 16:45:56 -0500

    Initial GraphQL Sandbox

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,45 @@ const path = require('path');
 
 const app = express();
 
+const { buildSchema } = require('graphql');
+const graphQLHTTP = require('express-graphql');
+const db = require('./models/postgresModel.js');
+
+// GraphQL Sandbox
+// Create a GraphQL Schema
+const qlSchema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+const rootResolvers = {
+  hello: () => {
+    return 'Hello world!';
+  }
+};
+
+app.use(
+  '/graphql',
+  graphQLHTTP({
+    schema: qlSchema,
+    rootValue: rootResolvers,
+    graphiql: true
+  })
+);
+
+// Database sandbox
+// app.use((req, res, next) => {
+//   console.log(req.body, 'body');
+
+//   // Test Query
+//   db.query('SELECT * FROM test', null, (err, result) => {
+//     console.log(result.rows);
+//   });
+
+//   next();
+// });
+
 // app.get('/', (req, res) => {
 //   res.sendFile(
 //     path.resolve(__dirname, '../', 'Frontend', 'public', 'index.html')
